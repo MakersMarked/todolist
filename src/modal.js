1d540518f7ed0bb93b42dfbd4cf0cc375531afd1
MakersMarked <127469497+MakersMarked@users.noreply.github.com>
2023-07-28 22:43:23 -0700

    Add Project logic and HTML content

---
+++ src/modal.js
@@ -0,0 +1,92 @@
+import { Project, ToDoItem } from './classes';
+import{ newElement} from './index';
+import { addProject, projectsLib,loadProjects } from './logic';
+import datepicker from 'js-datepicker';
+
+
+
+const ProjectModal = () => {
+    const bodyContainer = document.querySelector('#body-container');
+    function closeModal(){
+        modal.style.display ='none'
+    }
+const modal = newElement('div', bodyContainer );
+    modal.setAttribute('id', 'modal-container');
+    modal.classList.add('modal');
+    modal.style.display = 'block';
+    
+    const modalContent = newElement('div',modal);
+    modalContent.classList.add('modal-content')
+        const close = newElement('span', modalContent)
+            close.classList.add('close');
+            close.textContent = 'X';
+            close.addEventListener('click', closeModal)
+        const modalTitle = newElement('h3',modalContent);
+            modalTitle.classList.add('modal-title');
+            modalTitle.textContent = 'New Project';
+        const projectNameInput = newElement('input', modalContent);
+            projectNameInput.classList.add('input');
+            projectNameInput.placeholder = 'Name your Project';
+        const btnDiv = newElement('div',modalContent)
+            btnDiv.classList.add('btn')
+            const addBtn = newElement('button',btnDiv);
+            addBtn.textContent = 'Add Project';
+            addBtn.addEventListener('click',() => {
+                addProject(projectsLib, projectNameInput.value);
+                closeModal()
+                console.log(projectsLib)
+                // render()
+            })
+            const cancelBtn = newElement('button', btnDiv);
+            cancelBtn.textContent = 'Cancel';
+            cancelBtn.addEventListener('click',closeModal)
+}
+
+const ToDoModal = () => {
+    const htmlBody = document.querySelector('body');
+    function closeModal(){
+        modal.style.display ='none'
+    }
+    const modal = newElement('div', htmlBody );
+    modal.setAttribute('id', 'modal-container');
+    modal.classList.add('modal');
+    modal.style.display = 'block';
+    
+    const modalContent = newElement('div',modal);
+    modalContent.classList.add('modal-content')
+        const close = newElement('span', modalContent)
+            close.classList.add('close');
+            close.textContent = 'X';
+            close.addEventListener('click', closeModal)
+        const modalTitle = newElement('h3',modalContent);
+            modalTitle.classList.add('modal-title');
+            modalTitle.textContent = 'New To-Do Item';
+        const toDoNameInput = newElement('input', modalContent);
+            toDoNameInput.classList.add('input');
+            toDoNameInput.placeholder = 'Name your Project';
+        const dueDate = newElement('input',modalContent);
+            dueDate.classList.add('date')
+            dueDate.type = 'text'
+            const picker = datepicker('.date');
+        const descriptInput = newElement('textarea',modalContent);
+        const urgent = newElement('input', modalContent);
+            urgent.type = 'checkbox';
+        const btnDiv = newElement('div',modalContent)
+            btnDiv.classList.add('btn')
+            const addBtn = newElement('button',btnDiv);
+            addBtn.textContent = 'Add To-Do Item';
+            addBtn.addEventListener('click',() => {
+                console.log(
+                    toDoNameInput.value,
+                    descriptInput.value,
+                    dueDate.value,
+                    urgent.value);
+                closeModal()
+                console.log(projectsLib)
+                // render()
+            })
+            const cancelBtn = newElement('button', btnDiv);
+            cancelBtn.textContent = 'Cancel';
+            cancelBtn.addEventListener('click',closeModal)
+}
+export {ProjectModal, ToDoModal}
